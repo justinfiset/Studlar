6b8c426bcb71229ae90e836e2d6f94384f0cc218
Justin Fiset <justinfiset@hotmail.fr>
2025-04-06 13:03:22 -0400

    Bug fix and prevent icons selections

--- studlar/src/components/Boards/Board.js
+++ studlar/src/components/Boards/Board.js
@@ -14,13 +14,13 @@ export default function Board({ board, onDelete, onUpdate }) {
 
     const delCurrentBoard = async () => {
         try {
-            const respone = await fetch(
+            const response = await fetch(
                 `/api/boards/?id=${board.id}&owner_id=${user.id}`,
                 { method: "DELETE" }
             );
-            const data = await respone.json();
+            const data = await response.json();
 
-            if (respone.ok) {
+            if (response.ok) {
                 onDelete();
             } else {
                 console.error("Error:", data.error);
@@ -43,7 +43,7 @@ export default function Board({ board, onDelete, onUpdate }) {
     const handleDelete = () => {
         setShowOptions(false);
         delCurrentBoard();
-    }
+    };
 
     const updateBoard = async () => {
         try {
@@ -76,124 +76,119 @@ export default function Board({ board, onDelete, onUpdate }) {
     };
 
     const displayTasks = (board) => {
-        return board.task_lists.map((tasklist) => {
-            return (
-                <div key={tasklist.id}>
-                    {isEditing ? (
-                        <></>
-                    ) : (
-                        // Display the tasklist name only if it is not empty
-                        tasklist.name && (
-                            <div className={styles.subsectionHeader}>
-                                <p>{tasklist.name}</p>
-                            </div>
-                        )
-                    )}
-                    <div
-                        className="todolist-task"
-                        key={tasklist.id}
-                        draggable="true"
-                    >
-                        <span className="material-icons">
-                            radio_button_unchecked
-                        </span>
+        return board.task_lists.map((tasklist) => (
+            <div key={tasklist.id}>
+                {!isEditing && tasklist.name && (
+                    <div className={styles.subsectionHeader}>
                         <p>{tasklist.name}</p>
-                        <span className={`material-icons ${styles.dragIcon}`}>
-                            drag_indicator
-                        </span>
                     </div>
-                    <span></span>
+                )}
+                <div
+                    className="todolist-task"
+                    key={tasklist.id}
+                    draggable="true"
+                >
+                    <span className="material-icons">
+                        radio_button_unchecked
+                    </span>
+                    <p>{tasklist.name}</p>
+                    <span className={`material-icons ${styles.dragIcon}`}>
+                        drag_indicator
+                    </span>
                 </div>
-            );
-        });
+                <span></span>
+            </div>
+        ));
+    };
+
+    const displayTasklist = (board) => {
+        return (
+            <>
+                {board.task_lists.length > 0 && (
+                    <>
+                        {displayTasks(board)}
+                        <div className="todolist-task">
+                            <p>
+                                <strong>+ Ajouter un item</strong>
+                            </p>
+                            <span></span>
+                        </div>
+                    </>
+                )}
+            </>
+        );
     };
 
     const handleOptionsBtn = () => {
         setShowOptions(!showOptions);
-    }
+    };
 
     return (
-        <article>
+        <article className={styles.card}>
             <div className={styles.draggableHeader}></div>
             <div className="card-header">
-                <p>{board.name}</p>
-                <span className="material-icons card-header-menu" onClick={handleOptionsBtn}>
-                    {showOptions ? "close" : "menu"}
-                </span>
-            </div>
-            {!showOptions ? (
-                <>
-                    <div className={styles.draggableHeader}></div>
-                    <div className="card-header">
-                        {isEditing ? (
-                            <input
-                                type="text"
-                                placeholder="Board Name"
-                                id="boardName"
-                                value={boardName}
-                                onChange={(e) => setBoardName(e.target.value)}
-                            />
-                        ) : (
-                            <p>{board.name}</p>
-                        )}
-                        { isEditing && <button className={styles.saveBtn} onClick={handleEdit}>Save</button>}
+                {isEditing ? (
+                    <>
+                        <input
+                            type="text"
+                            placeholder="Board Name"
+                            className={styles.editInput}
+                            id="boardName"
+                            value={boardName}
+                            onChange={(e) => setBoardName(e.target.value)}
+                        />
+
+                        <button className={styles.saveBtn} onClick={handleEdit}>
+                            Save
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <p>{board.name}</p>
                         <span
                             className="material-icons card-header-menu"
                             onClick={handleOptionsBtn}
                         >
                             {showOptions ? "close" : "menu"}
                         </span>
-                    </div>
-                    {!showOptions ? (
-                        <>
-                            <p>{board.description}</p>
-                            {displayTasklist(board)}
-                        </>
-                    ) : (
-                        <div className={styles.options}>
-                            {board.positionX > 0 && (
-                                <div
-                                    className={styles.positionArrow}
-                                    onClick={moveCard.bind(this, -1)}
-                                >
-                                    <p className={`material-icons`}>
-                                        chevron_left
-                                    </p>
-                                </div>
-                            )}
-                            <div className={styles.optionsContainer}>
-                                <p
-                                    onClick={handleEdit}
-                                    className="material-icons"
-                                >
-                                    edit
-                                </p>
-                                <p
-                                    onClick={handleDelete}
-                                    className={`material-icons ${styles.deleteBtn}`}
-                                >
-                                    delete
-                                </p>
-                                {/* <p className="material-icons">share</p> */}
-                            </div>
-                            {board.positionX < maxXPosition && (
-                                <div
-                                    className={styles.positionArrow}
-                                    onClick={moveCard.bind(this, 1)}
-                                >
-                                    <p className={`material-icons`}>
-                                        chevron_right
-                                    </p>
-                                </div>
-                            )}
-                        </div>
-                    )}
+                    </>
+                )}
+            </div>
+
+            {!showOptions ? (
+                <>
+                    <p>{board.description}</p>
+                    {displayTasklist(board)}
                 </>
             ) : (
-                <div className={styles.optionsContainer}>
-                    <p onClick={handleEdit} className="material-icons">edit</p>
-                    <p onClick={handleDelete} className={`material-icons ${styles.deleteBtn}`}>delete</p>
-                    {/* <p className="material-icons">share</p> */}
+                <div className={styles.options}>
+                    {board.positionX > 0 && (
+                        <div
+                            className={styles.positionArrow}
+                            onClick={moveCard.bind(this, -1)}
+                        >
+                            <p className={`material-icons`}>chevron_left</p>
+                        </div>
+                    )}
+                    <div className={styles.optionsContainer}>
+                        <p onClick={handleEdit} className="material-icons">
+                            edit
+                        </p>
+                        <p
+                            onClick={handleDelete}
+                            className={`material-icons ${styles.deleteBtn}`}
+                        >
+                            delete
+                        </p>
+                    </div>
+                    {board.positionX < maxXPosition && (
+                        <div
+                            className={styles.positionArrow}
+                            onClick={moveCard.bind(this, 1)}
+                        >
+                            <p className={`material-icons`}>chevron_right</p>
+                        </div>
+                    )}
                 </div>
             )}
         </article>
